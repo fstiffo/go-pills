595d15f5c97c52a15d6cd1fa7469eaabf0bad756
Francesco Stiffoni <fstiffo@sgajo.com>
2025-08-09 09:41:56 +0200

    Fix: Missing field and column for “Last stocked” in prescription summary Last-refresh timestamp returns oldest log entry Incorrect “Stock in days” calculation Typographical error in header

--- model/log.go
+++ model/log.go
@@ -12,15 +12,14 @@ func LastRefresh(db *gorm.DB) string {
 		ConsumedAt time.Time
 	}
 	var cl ConsumptionLog
-	db.First(&cl)
+	db.Order("consumed_at desc").Select("consumed_at").First(&cl)
 	type StockLog struct {
 		StockedAt time.Time
 	}
 	var sl StockLog
-	db.First(&sl)
+	db.Order("stocked_at desc").Select("stocked_at").First(&sl)
 	if cl.ConsumedAt.After(sl.StockedAt) {
 		return cl.ConsumedAt.Format("2006-01-02 15:04:05")
 	}
 	return sl.StockedAt.Format("2006-01-02 15:04:05")
-
 }
--- model/prescription.go
+++ model/prescription.go
@@ -13,13 +13,14 @@ import (
 // GetPrescriptionsSummary returns a summary of all prescriptions
 func GetPrescriptionsSummary(db *gorm.DB) pterm.TableData {
 	tableData := pterm.TableData{
-		{"Active Igredient", "Dosage", "Frequency", "Valid from", "Last consumed", "Last stocked", "Stock in days"}}
+		{"Active Ingredient", "Dosage", "Frequency", "Valid from", "Last consumed", "Last stocked", "Stock in days"}}
 	type prescription struct {
 		Prescription
 		Name           string
 		Unit           string
 		Stock          int64
 		LastConsumedAt sql.NullTime
+		LastStockedAt  sql.NullTime
 	}
 	ps := []prescription{}
 	result := db.Model(&Prescription{}).
@@ -41,8 +42,9 @@ func GetPrescriptionsSummary(db *gorm.DB) pterm.TableData {
 		frequency := strconv.Itoa(p.DosingFrequency) + dayOrDays
 		validFrom := p.StartDate.Time.Format("2006-01-02")
 		lastConsumed := p.LastConsumedAt.Time.Format("2006-01-02")
-		stockInDays := strconv.FormatInt(p.Stock/int64(p.DosingFrequency)*int64(p.DosingFrequency), 10)
-		tableData = append(tableData, []string{name, dosage, frequency, validFrom, lastConsumed, stockInDays})
+		lastStocked := p.LastStockedAt.Time.Format("2006-01-02")
+		stockInDays := strconv.FormatInt(p.Stock/int64(p.Dosage)*int64(p.DosingFrequency), 10)
+		tableData = append(tableData, []string{name, dosage, frequency, validFrom, lastConsumed, lastStocked, stockInDays})
 	}
 
 	return tableData
