3253b2286989d536c01611bb5a87380180441308
Francesco Stiffoni <fstiffo@sgajo.com>
2025-08-09 16:08:43 +0200

    Fix error il LastRefresh

--- model/log.go
+++ model/log.go
@@ -8,18 +8,31 @@ import (
 
 // LastRefresh returns the last time the logs were refreshed.
 func LastRefresh(db *gorm.DB) string {
-	type ConsumptionLog struct {
-		ConsumedAt time.Time
-	}
-	var cl ConsumptionLog
-	db.Order("consumed_at desc").Select("consumed_at").First(&cl)
-	type StockLog struct {
-		StockedAt time.Time
-	}
+	var il IntakeLog
 	var sl StockLog
-	db.Order("stocked_at desc").Select("stocked_at").First(&sl)
-	if cl.ConsumedAt.After(sl.StockedAt) {
-		return cl.ConsumedAt.Format("2006-01-02 15:04:05")
+	intakeErr := db.Model(&IntakeLog{}).
+		Order("consumed_at desc").
+		Select("consumed_at").
+		First(&il).Error
+	stockErr := db.Model(&StockLog{}).
+		Order("stocked_at desc").
+		Select("stocked_at").
+		First(&sl).Error
+
+	var zero time.Time
+
+	if intakeErr != nil {
+		il.ConsumedAt = zero
+	}
+	if stockErr != nil {
+		sl.StockedAt = zero
+	}
+
+	if il.ConsumedAt.IsZero() && sl.StockedAt.IsZero() {
+		return "Never"
+	}
+	if il.ConsumedAt.After(sl.StockedAt) {
+		return il.ConsumedAt.Format("2006-01-02 15:04:05")
 	}
 	return sl.StockedAt.Format("2006-01-02 15:04:05")
 }
